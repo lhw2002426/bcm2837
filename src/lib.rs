83d41444d1869cbb393c50e8be4113756a84772f
lhw2002426 <1466397747@qq.com>
2023-07-01 07:30:33 +0800

    only emmc

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,20 @@
 #![no_std]
-#![feature(asm)]
+//#![feature(asm)]
 #![allow(unused_parens)]
 
 extern crate volatile;
 #[macro_use]
 extern crate log;
-extern crate aarch64;
+//extern crate aarch64;
 
 pub mod addr;
 pub mod atags;
-pub mod dma;
+//pub mod dma;
 pub mod emmc;
-pub mod gpio;
+//pub mod gpio;
 pub mod interrupt;
 pub mod mailbox;
-pub mod mini_uart;
-pub mod pwm;
+//pub mod mini_uart;
+//pub mod pwm;
 pub mod qa7_control;
-pub mod timer;
+//pub mod timer;
